79d113fbbe2aa1904ab59a25c9eec742ec7c2613
Savyashree <54622190+Savyashree@users.noreply.github.com>
2023-02-24 18:15:04 +0530

    Added simple redux code to fetch api data

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import './style.css';
 import { act } from 'react-dom/test-utils';
-import { createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import axios from 'axios';
 
+const logger = createLogger();
 export default function App() {
   return (
     <div>
@@ -12,23 +16,48 @@ export default function App() {
   );
 }
 
-const reducer = function (state, actions) {
-  switch (actions.type) {
-    case 'INC':
-      state = state + actions.payload;
+const initialState = {
+  userData: {},
+  fetching: false,
+  fetched: false,
+  error: null,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CALL_API':
+      state = { ...state, fetching: true };
       break;
-    case 'DEC':
-      state = state - actions.payload;
+    case 'RECEIVED_API':
+      state = {
+        ...state,
+        fetching: false,
+        fetched: true,
+        userData: action.payload,
+      };
       break;
+    case 'ERROR':
+      state = { ...state, error: action.payload };
   }
   return state;
 };
 
-const store = createStore(reducer, 0);
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(reducer, 0, middleware);
 
 store.subscribe(() => {
-  console.log('state changed', store.getState());
+  console.log('state updated :', store.getState());
 });
 
-store.dispatch({ type: 'INC', payload: 2 });
-store.dispatch({ type: 'DEC', payload: 2 });
+store.dispatch((dispatch) => {
+  dispatch({ type: 'CALL_API' });
+  fetch('https://dummyjson.com/products/1')
+    .then((res) => {
+      return res.json();
+    })
+    .then((val) => {
+      dispatch({ type: 'RECEIVED_API', payload: val });
+    })
+    .catch((e) => {
+      dispatch({ type: 'ERROR', payload: e });
+    });
+});
